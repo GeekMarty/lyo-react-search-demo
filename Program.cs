c773cb68ff35120fa5a7d88bb11f3ccd6effe21a
Rok Zontar <rok.zontar@gmail.com>
2017-01-27 08:08:02 +0100

    Add IIS Integration for Azure deployment

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace WebApplication
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
